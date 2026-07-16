4e9b35cee0dfeed09475869bcdca817c8ecfeb69
Kevin <kevin@fndryx.io>
2026-07-15 21:23:04 -0400

    add /unsubscribe rewrite to supabase edge function

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/unsubscribe",
+        destination:
+          "https://xcesazshfinwsdqyndlg.supabase.co/functions/v1/unsubscribe",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
